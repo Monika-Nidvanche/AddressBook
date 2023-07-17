42474a6905cfff5850d63ae5b7f99a8c309fb2f1
Monika-Nidvanche <monikanidvanche5@gmail.com>
2023-07-17 20:18:03 +0530

    Welcome to AddressBook

---
+++ AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to address book");
+		AddressBookImp.getAddressBook();
+
+	}
+
+}
